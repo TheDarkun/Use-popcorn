498099665c467d87b341c82651bc13104fae9071
Darkun <106868917+TheDarkun@users.noreply.github.com>
2024-01-21 20:16:26 +0100

    add search eventListener with ref

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,32 @@
-﻿export default function Search({query, setQuery}) {
+﻿import {useEffect, useRef} from "react";
+
+export default function Search({query, setQuery}) {
+    
+    const inputEl = useRef();
+    
+    useEffect(() => {
+
+        const callback = (e) => {
+            if(e.code === "Enter" && document.activeElement !== inputEl.current) {
+                setQuery("");
+                inputEl.current.focus();
+            }
+        }
+        
+        document.addEventListener("keydown", callback)
+        
+        return () => document.removeEventListener("keydown", callback);
+    }, [])
+    
     return (
         <input
+            autoFocus
             className="search"
             type="text"
             placeholder="Search movies..."
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            ref={inputEl}
         />
     )
 }
